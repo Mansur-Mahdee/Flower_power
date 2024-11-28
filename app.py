6f16ed3161e0e1efe509fd8360c8d93bcd54d0b6
Mansur-Mahdee <116994740+Mansur-Mahdee@users.noreply.github.com>
2024-11-27 21:34:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ def check_and_download_dataset():
     else:
         return download_kaggle_dataset()
 
-@st.cache_resource
 # Function to extract dataset
 def extract_dataset():
     # Path to the downloaded zip file and extraction directory
